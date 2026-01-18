914f715f2cab33967f7383e57fee8e28ddbc155d
ItsSonchan <sonja.grund@icloud.com>
2026-01-18 06:34:34 -0500

    fixed some errors in the code

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -73,7 +73,7 @@ const ConferenceEvent = () => {
             })
         }
         else if (section === "meals"){
-            mealsItems.forEach((items)=>{
+            mealsItems.forEach((item)=>{
                 if(item.selected){
                     totalCost+=item.cost*numberOfPeople;
                 }
@@ -174,47 +174,41 @@ const ConferenceEvent = () => {
         <div className="total_cost">Total Cost: ${venueTotalCost}</div>
       </div>
 
-                          
+             {/*Necessary Add-ons*/}             
         <div id="addons" className="venue_container container_main">
         <div className="text">
         <h1> Add-ons Selection</h1>
         </div>
         <div className="addons_selection">  
-        {avItems.map((item,index)=>{
+        {avItems.map((item,index)=>(
         <div className="av_data venue_main" key={index}>
         <div className="img">
         <img src={item.img} alt={item.name}/>
         </div>
         <div className="text">{item.name}</div>
-        <div>{item.cost}</div>
+        <div>${item.cost}</div>
         <div className="addons_btn">
-        <button className="btn-warning" onClick={()=>handleDecrementAvQuantity(index)}>-</button>
+        <button className="btn-warning" onClick={()=>handleDecrementAvQuantity(index)}>&ndash;</button>
         <span className="quantity-value">{item.quantity}</span>
-        <button className="btn-success" onClick={()=>handleIncrementAvQuantity(index)}>+</button>
+        <button className="btn-success" onClick={()=>handleIncrementAvQuantity(index)}>&#43;</button>
         </div>
         </div>
-        })}
+        ))}
         </div>
         <div className="total_cost">Total Cost: {avTotalCost}</div>
         </div>
 
-                            {/* Meal Section */}
-
-                            <div className="input-container venue_selection">
-                                <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
-                                <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
-                                onChange={(e)=>setNumberOfPeople(parseInt(e.target.value))} min="1"/>
-                                </div>
-
+                            {/* Meal Section */}  
                             <div id="meals" className="venue_container container_main">
-
                                 <div className="text">
 
                                     <h1>Meals Selection</h1>
                                 </div>
-
+                                
                                 <div className="input-container venue_selection">
-
+                                <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
+                            <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                                onChange={(e)=>setNumberOfPeople(parseInt(e.target.value))} min="1"/>
                                 </div>
                                 <div className="meal_selection">
                                 {mealsItems.map((item,index)=>(
